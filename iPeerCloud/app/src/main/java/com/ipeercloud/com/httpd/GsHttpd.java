3eb5f67acbb62f08325fcdfed4a1f6c622cb9a8a
huangkuncan <673391138@qq.com>
2017-05-21 13:50:34 +0800

    音视频调试

--- iPeerCloud/app/src/main/java/com/ipeercloud/com/httpd/GsHttpd.java
+++ iPeerCloud/app/src/main/java/com/ipeercloud/com/httpd/GsHttpd.java
@@ -1,11 +1,9 @@
 package com.ipeercloud.com.httpd;
 
 import com.ipeercloud.com.controler.GsSocketManager;
-import com.ipeercloud.com.utils.GsFile;
 import com.ipeercloud.com.utils.GsLog;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 
 import fi.iki.elonen.NanoHTTPD;
 
@@ -16,54 +14,51 @@ import fi.iki.elonen.NanoHTTPD;
  */
 
 public class GsHttpd extends NanoHTTPD {
-    public static  int fileSize = 0;
-    public static  int bufSize = 1024 * 1024;
-    private String fileName = "hkc.mp4";
+    public static int fileSize = 0;
+    public static int bufSize = 1024 * 1024;
     public static String sRemotePath;
+
     public GsHttpd(int port) {
         super(8080);
     }
-
     @Override
     public Response serve(IHTTPSession session) {
+        if (fileSize < 5 * 1024 * 1024) {
+            bufSize = 1024 * 1024;
+        } else {
+            bufSize = 1024 * 1024;
+        }
         String range = session.getHeaders().get("range");
-        GsLog.d("请求range " + range);
+        GsLog.d("\n ================================ \n 请求range " + range);
         int start = getStart(range);
-        byte[] buf = getFromJni(start,sRemotePath);
+        byte[] buf = getFromJni(start, sRemotePath);
         Response response = new Response(NanoHTTPD.Response.Status.PARTIAL_CONTENT, "text/html", new ByteArrayInputStream(buf));
         response.addHeader("Content-Length", "" + bufSize);
-        response.addHeader("Content-Range", "bytes " + start + "-" + (start+bufSize)+ "/" + fileSize);
+        GsLog.d(" Content-Range response:  " + "bytes " + start + "-" + (start + bufSize) + "/" + fileSize);
+        response.addHeader("Content-Range", "bytes " + start + "-" + (start + bufSize) + "/" + fileSize);
         return response;
     }
 
 
-
-    private byte[] getFromJni(int start,String remotePath) {
-        byte[] buf = new byte[bufSize+2];
+    private synchronized byte[] getFromJni(int start, String remotePath) {
+        byte[] buf = new byte[bufSize + 2];
         int[] leng = new int[5];
-        GsLog.d("远端路径 "+remotePath);
+        GsLog.d("远端路径 " + remotePath);
         leng[0] = bufSize;//要读取的字节数要先保存到这里
         int result = GsSocketManager.getInstance().gsReadFileBuffer(remotePath, start, bufSize, buf, leng);
         if (result == 0) {
-            GsLog.d("请求数据流成功  "+buf.length);
+            GsLog.d("请求数据流成功  " + buf.length);
             return buf;
         }
         GsLog.d("请求数据失败");
-        return null;
+        return new byte[]{0};
     }
-    private int getStart(String range){
+
+    private int getStart(String range) {
         int index = range.indexOf("-");
-        String num = range.substring(6,index);
-        GsLog.d("num "+num);
+        String num = range.substring(6, index);
         return Integer.parseInt(num);
     }
-    private long getSize() {
-        File file = new File(GsFile.getDir(), fileName);
-        if (!file.exists()) {
-            GsLog.d("服务端文件不存在");
-            return 0;
-        }
-        return file.length();
-    }
+
 
 }
--- iPeerCloud/app/src/main/java/com/ipeercloud/com/view/activity/VideoViewActivity.java
+++ iPeerCloud/app/src/main/java/com/ipeercloud/com/view/activity/VideoViewActivity.java
@@ -29,6 +29,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ipeercloud.com.R;
+import com.ipeercloud.com.utils.GsLog;
+
+import java.io.IOException;
 
 import io.vov.vitamio.MediaPlayer;
 import io.vov.vitamio.MediaPlayer.OnBufferingUpdateListener;
@@ -37,106 +40,123 @@ import io.vov.vitamio.Vitamio;
 import io.vov.vitamio.widget.MediaController;
 import io.vov.vitamio.widget.VideoView;
 
-import static io.vov.vitamio.MediaPlayer.VIDEOQUALITY_HIGH;
-
 public class VideoViewActivity extends Activity implements OnInfoListener, OnBufferingUpdateListener {
 
-  /**
-   * TODO: Set the path variable to a streaming video URL or a local media file
-   * path.
-   */
-  private String path="http://localhost:8080/";
-  private Uri uri;
-  private VideoView mVideoView;
-  private ProgressBar pb;
-  private TextView downloadRateView, loadRateView;
-  private ViewGroup mViewGp;
+    /**
+     * TODO: Set the path variable to a streaming video URL or a local media file
+     * path.
+     */
+    private String path = "http://localhost:8080/";
+    private Uri uri;
+    private VideoView mVideoView;
+    private ProgressBar pb;
+    private TextView downloadRateView, loadRateView;
+    private ViewGroup mViewGp;
 
-  @Override
-  public void onCreate(Bundle icicle) {
-    super.onCreate(icicle);
-		Vitamio.isInitialized(getApplicationContext());
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        Vitamio.isInitialized(getApplicationContext());
 
-    setContentView(R.layout.videobuffer);
-    mVideoView = (VideoView) findViewById(R.id.buffer);
-    pb = (ProgressBar) findViewById(R.id.probar);
+        setContentView(R.layout.videobuffer);
+        mVideoView = (VideoView) findViewById(R.id.buffer);
+        pb = (ProgressBar) findViewById(R.id.probar);
 
-    downloadRateView = (TextView) findViewById(R.id.download_rate);
-    loadRateView = (TextView) findViewById(R.id.load_rate);
+        downloadRateView = (TextView) findViewById(R.id.download_rate);
+        loadRateView = (TextView) findViewById(R.id.load_rate);
 
-    // 获取path
-    Intent intent = getIntent();
-    path = path+intent.getStringExtra("path");
-    if (TextUtils.isEmpty(path)) {
-      // Tell the user to provide a media file URL/path.
-      Toast.makeText(
-          VideoViewActivity.this,
-          "Please edit VideoBuffer Activity, and set path"
-              + " variable to your media file URL/path", Toast.LENGTH_LONG).show();
-      return;
-    } else {
-      /*
-       * Alternatively,for streaming media you can use
-       * mVideoView.setVideoURI(Uri.parse(URLstring));
-       */
-      uri = Uri.parse(path);
-      mVideoView.setVideoURI(uri);
-      mVideoView.setMediaController(new MediaController(this));
-      mVideoView.requestFocus();
-      mVideoView.setVideoQuality(VIDEOQUALITY_HIGH);
-      mVideoView.setOnInfoListener(this);
-      mVideoView.setOnBufferingUpdateListener(this);
-      mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-        @Override
-        public void onPrepared(MediaPlayer mediaPlayer) {
-          // optional need Vitamio 4.0
-          mediaPlayer.setPlaybackSpeed(1.0f);
+        // 获取path
+        Intent intent = getIntent();
+        path = path + intent.getStringExtra("path");
+        if (TextUtils.isEmpty(path)) {
+            // Tell the user to provide a media file URL/path.
+            Toast.makeText(
+                    VideoViewActivity.this,
+                    "Please edit VideoBuffer Activity, and set path"
+                            + " variable to your media file URL/path", Toast.LENGTH_LONG).show();
+            return;
         }
-      });
+
+        GsLog.d("进入 videoViewActivity");
+        /*if (path.contains("mp3") || path.contains("MP3")) {
+            playAudio(path);
+            return;
+        }*/
+        uri = Uri.parse(path);
+//      uri = Uri.parse("http://www.modrails.com/videos/passenger_nginx.mov");
+        mVideoView.setVideoURI(uri);
+        mVideoView.setMediaController(new MediaController(this));
+        mVideoView.requestFocus();
+//      mVideoView.setVideoQuality(VIDEOQUALITY_HIGH);
+        mVideoView.setZOrderOnTop(true);
+        mVideoView.setOnInfoListener(this);
+        mVideoView.setOnBufferingUpdateListener(this);
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                // optional need Vitamio 4.0
+                mediaPlayer.setPlaybackSpeed(1.0f);
+            }
+        });
+
+
     }
 
-  }
-  public static void startActivity(Context context,String fileName){
-    Intent intent = new Intent(context,VideoViewActivity.class);
-    intent.putExtra("path",fileName);
-    context.startActivity(intent);
-  }
-  @Override
-  public boolean onInfo(MediaPlayer mp, int what, int extra) {
-    switch (what) {
-    case MediaPlayer.MEDIA_INFO_BUFFERING_START:
-      if (mVideoView.isPlaying()) {
-        mVideoView.pause();
-        pb.setVisibility(View.VISIBLE);
-        downloadRateView.setText("");
-        loadRateView.setText("");
-        downloadRateView.setVisibility(View.VISIBLE);
-        loadRateView.setVisibility(View.VISIBLE);
-
-      }
-      break;
-    case MediaPlayer.MEDIA_INFO_BUFFERING_END:
-      mVideoView.start();
-      pb.setVisibility(View.GONE);
-      downloadRateView.setVisibility(View.GONE);
-      loadRateView.setVisibility(View.GONE);
-      break;
-    case MediaPlayer.MEDIA_INFO_DOWNLOAD_RATE_CHANGED:
-      downloadRateView.setText("" + extra + "kb/s" + "  ");
-      break;
+    private void playAudio(String path) {
+        GsLog.d("播放音乐");
+        MediaPlayer mp = new MediaPlayer(this);
+        try {
+            mp.setDataSource(path);
+            mp.prepare();
+            mp.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            GsLog.d("播放音乐出现了异常");
+        }
     }
-    return true;
-  }
 
-  @Override
-  protected void onDestroy() {
-    setResult(100);
-    super.onDestroy();
-  }
+    public static void startActivity(Context context, String fileName) {
+        Intent intent = new Intent(context, VideoViewActivity.class);
+        intent.putExtra("path", fileName);
+        context.startActivity(intent);
+    }
 
-  @Override
-  public void onBufferingUpdate(MediaPlayer mp, int percent) {
-    loadRateView.setText(percent + "%");
-  }
+    @Override
+    public boolean onInfo(MediaPlayer mp, int what, int extra) {
+        switch (what) {
+            case MediaPlayer.MEDIA_INFO_BUFFERING_START:
+                if (mVideoView.isPlaying()) {
+                    mVideoView.pause();
+                    pb.setVisibility(View.VISIBLE);
+                    downloadRateView.setText("");
+                    loadRateView.setText("");
+                    downloadRateView.setVisibility(View.VISIBLE);
+                    loadRateView.setVisibility(View.VISIBLE);
+
+                }
+                break;
+            case MediaPlayer.MEDIA_INFO_BUFFERING_END:
+                mVideoView.start();
+                pb.setVisibility(View.GONE);
+                downloadRateView.setVisibility(View.GONE);
+                loadRateView.setVisibility(View.GONE);
+                break;
+            case MediaPlayer.MEDIA_INFO_DOWNLOAD_RATE_CHANGED:
+                downloadRateView.setText("" + extra + "kb/s" + "  ");
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        setResult(100);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        loadRateView.setText(percent + "%");
+    }
 
 }
